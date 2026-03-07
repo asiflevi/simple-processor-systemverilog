165058d41c356d3ce349dbdf6089be8c100e3c35
Asif <130288890+asiflevi@users.noreply.github.com>
2026-03-07 19:44:34 +0200

    Update regn.sv

--- regn.sv
+++ regn.sv
@@ -1,24 +1,12 @@
-module dec3to8(
-    input  logic [2:0] din,
-    input  logic       en,
-    output logic [7:0] dout
-);
+module regn #(parameter int w = 9)(input logic clk,input logic resetb , input logic [w-1:0] din, input logic en,output logic [w-1:0] Q);
 
-always_comb begin
-    dout = 8'b0;
-    if (en) begin
-        case (din)
-            3'b000: dout = 8'b00000001;
-            3'b001: dout = 8'b00000010;
-            3'b010: dout = 8'b00000100;
-            3'b011: dout = 8'b00001000;
-            3'b100: dout = 8'b00010000;
-            3'b101: dout = 8'b00100000;
-            3'b110: dout = 8'b01000000;
-            3'b111: dout = 8'b10000000;
-            default: dout = 8'b0;
-        endcase
-    end
+logic [w-1:0] D;
+assign D = en ? din : Q;
+
+always_ff@(posedge clk or negedge resetb) begin
+	if(!resetb)
+		Q<='0;
+	else
+		Q<=D;
 end
-
 endmodule
